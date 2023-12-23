b18ce58170d65987ba10aa057c223cab4da859f8
Wacker <kab1634@ya.ru>
2023-12-23 08:21:30 +0300

    main

---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Дмитрий',
+	imageBackground: true,
+	openInNewTab: false,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Доброе утро,',
+	greetingAfternoon: 'Добрый день,',
+	greetingEvening: 'Добрый вечер,',
+	greetingNight: 'Доброй ночи,',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: 'ac07988c51ca4cf112b2482147e18399', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'ru', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '59.9',
+	defaultLongitude: '30',
+
+	// Autochange
+	autoChangeTheme: false,
+
+	// Autochange by OS
+	changeThemeByOS: false,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'music',
+			link: 'https://music.yandex.ru/home',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://mail.yandex.ru/',
+		},
+		{
+			id: '3',
+			name: 'Vk',
+			icon: 'instagram',
+			link: 'https://vk.com',
+		},
+		{
+			id: '4',
+			name: 'Docs',
+			icon: 'file',
+			link: 'https://docs.google.com/document/u/0/',
+		},
+		{
+			id: '5',
+			name: 'iCloud',
+			icon: 'cloud',
+			link: 'https://www.icloud.com/',
+		},
+		{
+			id: '6',
+			name: 'Youtube',
+			icon: 'youtube',
+			link: 'https://youtube.com/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://music.yandex.ru/home',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'briefcase',
+			id: '1',
+			links: [
+				{
+					name: 'First Step',
+					link: 'https://stepik.org/course/67/promo?search=1599287167#toc',
+				},
+				{
+					name: 'Second Step',
+					link: 'https://stepik.org/course/512/promo',
+				},
+				{
+					name: 'Third Step',
+					link: 'https://stepik.org/course/122813/promo',
+				},
+				{
+					name: 'Final Boss',
+					link: 'https://leetcode.com/problemset/all/',
+				},
+			],
+		},
+		{
+			icon: 'hard-drive',
+			id: '2',
+			links: [
+				{
+					name: 'Mine',
+					link: 'https://drive.google.com/drive/my-drive',
+				},
+				{
+					name: '1',
+					link: 'https://drive.google.com/drive/folders/1stojC8o6qE4pTUCdL_nOcxS-6PC9fx4_',
+				},
+				{
+					name: '2',
+					link: 'https://drive.google.com/drive/folders/1zGAmFJ2105otV_T5c2Hep6PgNeKW_HWM',
+				},
+				{
+					name: 'Chat',
+					link: 'https://chat.openai.com/chat',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
